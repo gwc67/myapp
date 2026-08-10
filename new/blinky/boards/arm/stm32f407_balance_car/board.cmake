481e020e78d4ef9882e0e99a8fa4447c98293db6
gwc <3416218577@qq.com>
2026-08-10 14:40:35 +0800

    可以调试了

--- new/blinky/boards/arm/stm32f407_balance_car/board.cmake
+++ new/blinky/boards/arm/stm32f407_balance_car/board.cmake
@@ -1,3 +1,9 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")   
 
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+
+
+# 删除 stlink_gdbserver 相关行，改为 OpenOCD
+board_runner_args(openocd --cmd-pre-init "source [find interface/stlink.cfg]")
+board_runner_args(openocd --cmd-pre-init "source [find target/stm32f4x.cfg]")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
