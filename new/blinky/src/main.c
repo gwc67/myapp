0a1d761aa78452eea02209907c0efcf4b1d86eed
gwc <3416218577@qq.com>
2026-08-10 19:22:13 +0800

    oled 测试完毕

--- new/blinky/src/main.c
+++ new/blinky/src/main.c
@@ -23,21 +23,22 @@
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
+static const struct device *s_oled_pst = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
 
 
 int main(void)
 {
-	if(!device_is_ready(display))
+	if(!device_is_ready(s_oled_pst))
 	{
 		printk("SSD1306 is not ready\n");
 	}
 
-	cfb_framebuffer_init(display);
-	cfb_framebuffer_clear(display, true);
-	cfb_framebuffer_invert(display);
-	cfb_print(display, "hello zephyr", 0, 0);
-	cfb_framebuffer_finalize(display);
+	cfb_framebuffer_init(s_oled_pst);
+	cfb_framebuffer_clear(s_oled_pst, true);
+	cfb_framebuffer_set_font(s_oled_pst,0);
+	// cfb_framebuffer_invert(s_oled_pst);
+	cfb_print(s_oled_pst, "hello zephyr", 0, 0);
+	cfb_framebuffer_finalize(s_oled_pst);
 	while (1) {
 
 	}
