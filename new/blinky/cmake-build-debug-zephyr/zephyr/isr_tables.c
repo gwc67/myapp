8dcd37a895286c7a64c1e03d0f2e7c67e978914e
gwc <3416218577@qq.com>
2026-08-11 22:08:21 +0800

    dma通路还没有通

--- new/blinky/cmake-build-debug-zephyr/zephyr/isr_tables.c
+++ new/blinky/cmake-build-debug-zephyr/zephyr/isr_tables.c
@@ -65,24 +65,24 @@ const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 3 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 4 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 5 */
-	{(const void *)0x8019ea4, (ISR)0x8005ff1}, /* 6 */
-	{(const void *)0x8019ea0, (ISR)0x8005ff1}, /* 7 */
-	{(const void *)0x8019e9c, (ISR)0x8005ff1}, /* 8 */
-	{(const void *)0x8019e98, (ISR)0x8005ff1}, /* 9 */
-	{(const void *)0x8019e94, (ISR)0x8005ff1}, /* 10 */
-	{(const void *)0x8013ed4, (ISR)0x8011d07}, /* 11 */
-	{(const void *)0x8013ed4, (ISR)0x8011d11}, /* 12 */
-	{(const void *)0x8013ed4, (ISR)0x8011d1b}, /* 13 */
-	{(const void *)0x8013ed4, (ISR)0x8011d25}, /* 14 */
-	{(const void *)0x8013ed4, (ISR)0x8011d2f}, /* 15 */
-	{(const void *)0x8013ed4, (ISR)0x8011d39}, /* 16 */
-	{(const void *)0x8013ed4, (ISR)0x8011d43}, /* 17 */
+	{(const void *)0x8019b38, (ISR)0x8005c8d}, /* 6 */
+	{(const void *)0x8019b34, (ISR)0x8005c8d}, /* 7 */
+	{(const void *)0x8019b30, (ISR)0x8005c8d}, /* 8 */
+	{(const void *)0x8019b2c, (ISR)0x8005c8d}, /* 9 */
+	{(const void *)0x8019b28, (ISR)0x8005c8d}, /* 10 */
+	{(const void *)0x8013b70, (ISR)0x80119a3}, /* 11 */
+	{(const void *)0x8013b70, (ISR)0x80119ad}, /* 12 */
+	{(const void *)0x8013b70, (ISR)0x80119b7}, /* 13 */
+	{(const void *)0x8013b70, (ISR)0x80119c1}, /* 14 */
+	{(const void *)0x8013b70, (ISR)0x80119cb}, /* 15 */
+	{(const void *)0x8013b70, (ISR)0x80119d5}, /* 16 */
+	{(const void *)0x8013b70, (ISR)0x80119df}, /* 17 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 18 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 19 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 20 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 21 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 22 */
-	{(const void *)0x8019e90, (ISR)0x8005ff1}, /* 23 */
+	{(const void *)0x8019b24, (ISR)0x8005c8d}, /* 23 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 24 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 25 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 26 */
@@ -90,21 +90,21 @@ const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 28 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 29 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 30 */
-	{(const void *)0x8013f14, (ISR)0x801241b}, /* 31 */
-	{(const void *)0x8013f14, (ISR)0x8012423}, /* 32 */
+	{(const void *)0x8013bb0, (ISR)0x80120b7}, /* 31 */
+	{(const void *)0x8013bb0, (ISR)0x80120bf}, /* 32 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 33 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 34 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 35 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 36 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 37 */
-	{(const void *)0x8013ef4, (ISR)0x8013671}, /* 38 */
+	{(const void *)0x8013b90, (ISR)0x801330d}, /* 38 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 39 */
-	{(const void *)0x8019e8c, (ISR)0x8005ff1}, /* 40 */
+	{(const void *)0x8019b20, (ISR)0x8005c8d}, /* 40 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 41 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 42 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 43 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 44 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 45 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 46 */
-	{(const void *)0x8013ed4, (ISR)0x8011d4d}, /* 47 */
+	{(const void *)0x8013b70, (ISR)0x80119e9}, /* 47 */
 };
--- new/blinky/src/main.c
+++ new/blinky/src/main.c
@@ -16,6 +16,8 @@
 #include "menu/menu.h"
 #include "zephyr/drivers/uart.h"
 #include "zephyr/sys/ring_buffer.h"
+#include "zephyr/sys/time_units.h"
+#include "zephyr/syscalls/uart.h"
 
 #define UART_NODE DT_NODELABEL(usart2)
 
@@ -25,44 +27,90 @@ static const struct device *uart_dev_pst = DEVICE_DT_GET(UART_NODE);
 #define RX_BUF_SIZE 128
 #define TX_BUF_SIZE 128
 
+/* RX 双缓冲轮转 */
+static uint8_t rx_buf_a[RX_BUF_SIZE];
+// static uint8_t rx_buf_b[RX_BUF_SIZE];
+
+/* TX 线性缓冲 DMA需要连续内存，这个决定DMA发送下，无法使用tx环形缓冲区 */
+static uint8_t tx_buf[TX_BUF_SIZE];
+static volatile bool tx_busy;  //DMA正在发送标志位
+
+
+/* ISR -> main 传递 */
 static uint8_t rx_ring_buf_puc[RX_BUF_SIZE];
-static uint8_t tx_ring_buf_puc[TX_BUF_SIZE];
-
 static struct ring_buf rx_ring;
-static struct ring_buf tx_ring;
 
-static void uart_isr(const struct device *dev, void *user_data);
-static void send_next_data(void);
+//异步回调函数
+static void uart_async_cb(const struct device* dev,struct uart_event* evt,void *user_data)
+{
+	switch (evt->type) {
+		case UART_RX_RDY:
+			/* data.rx.buf + evt.data.rx.offset 指向有效数据的起始 */
+			/* evt->data.rx.len 是本批有效长度 */
+			ring_buf_put(&rx_ring, &evt->data.rx.buf[evt->data.rx.offset], evt->data.rx.len);
+			break;
+			//当前缓冲区已满，启动下一个缓冲区
+		case UART_RX_BUF_RELEASED:
+			uart_rx_buf_rsp(dev, evt->data.rx_buf.buf, RX_BUF_SIZE);
+			break;
+		case UART_TX_DONE:
+			tx_busy = false;
+			break;
+			//中断被中止
+		case UART_TX_ABORTED:
+			tx_busy = false;
+			printk("TX aborted %d bytes\n",evt->data.tx.len);
+		default:
+			break;
+	}
+}
+
+// static void uart_isr(const struct device *dev, void *user_data);
+// static void send_next_data(void);
 
 int main(void)
 {
 	ring_buf_init(&rx_ring, RX_BUF_SIZE, rx_ring_buf_puc);
-	ring_buf_init(&tx_ring, TX_BUF_SIZE, tx_ring_buf_puc);
+
 
 	if (!device_is_ready(uart_dev_pst)) {
 		printk("UART device not ready\n");
 		return -1;
 	}
 
+
+	int ret = uart_callback_set(uart_dev_pst, uart_async_cb, NULL);
+
+	if (ret ) {
+		printk("uart_callback_failed: %d\r\n",ret);
+		return ret;
+	}
+
+	uart_rx_enable(uart_dev_pst, rx_buf_a, RX_BUF_SIZE,SYS_FOREVER_US);
+	
+	
 	/* ── IT 中断驱动模式 ── */
 	/* 1. 注册 ISR 回调 (不是 uart_callback_set!) */
-	uart_irq_callback_user_data_set(uart_dev_pst, uart_isr, NULL);
+	// uart_irq_callback_user_data_set(uart_dev_pst, uart_isr, NULL);
 
 	/* 2. 使能 RX 中断 (不是 uart_rx_enable!) */
-	uart_irq_rx_enable(uart_dev_pst);
+	// uart_irq_rx_enable(uart_dev_pst);
 	// 使能tx中断
 
 	// uart_irq_tx_enable(uart_dev_pst);
 
-	printk("UART IT mode started, waiting for data...\n");
+	// printk("UART IT mode started, waiting for data...\n");
 
 	while (1) {
 		uint8_t ch;
 
 		/* 从 RX ring_buf 取出字节 → 回显到 TX */
 		while (ring_buf_get(&rx_ring, &ch, 1) == 1) {
-			ring_buf_put(&tx_ring, &ch, 1);
-			send_next_data();
+			if (!tx_busy) {
+				tx_buf[0] = ch;
+				tx_busy = true;
+				uart_tx(uart_dev_pst,tx_buf,1,SYS_FOREVER_US);
+			}
 		}
 
 		menu_task_v();
@@ -71,45 +119,45 @@ int main(void)
 	return 0;
 }
 
-static void send_next_data(void)
-{
-	 uint8_t ch;
+// static void send_next_data(void)
+// {
+// 	 uint8_t ch;
 
-    // 尝试填充 FIFO
-    while (ring_buf_peek(&tx_ring, &ch, 1) == 1) {
-        if (uart_fifo_fill(uart_dev_pst, &ch, 1) == 1) {
-            ring_buf_get(&tx_ring, &ch, 1); // 成功才消费
-        } else {
-            break; // FIFO 满，等下次 TX 中断
-        }
-    }
+//     // 尝试填充 FIFO
+//     while (ring_buf_peek(&tx_ring, &ch, 1) == 1) {
+//         if (uart_fifo_fill(uart_dev_pst, &ch, 1) == 1) {
+//             ring_buf_get(&tx_ring, &ch, 1); // 成功才消费
+//         } else {
+//             break; // FIFO 满，等下次 TX 中断
+//         }
+//     }
 
-	if (ring_buf_is_empty(&tx_ring)) {
-        uart_irq_tx_disable(uart_dev_pst);   // 没数据了，关中断防止空转
-    } else {
-        uart_irq_tx_enable(uart_dev_pst);    // 还有数据但 FIFO 满，保持中断等待排水
-    }
-}
+// 	if (ring_buf_is_empty(&tx_ring)) {
+//         uart_irq_tx_disable(uart_dev_pst);   // 没数据了，关中断防止空转
+//     } else {
+//         uart_irq_tx_enable(uart_dev_pst);    // 还有数据但 FIFO 满，保持中断等待排水
+//     }
+// }
 
-/* ── IT 中断驱动 ISR ──
- * 每收到一个字节硬件就触发一次中断
- * uart_irq_update() 读取硬件状态寄存器
- * uart_irq_rx_ready() 检查 RX 是否有数据
- * uart_fifo_read() 从硬件 FIFO/DR 读取字节
- */
-static void uart_isr(const struct device *dev, void *user_data)
-{
-	uart_irq_update(dev);
+// /* ── IT 中断驱动 ISR ──
+//  * 每收到一个字节硬件就触发一次中断
+//  * uart_irq_update() 读取硬件状态寄存器
+//  * uart_irq_rx_ready() 检查 RX 是否有数据
+//  * uart_fifo_read() 从硬件 FIFO/DR 读取字节
+//  */
+// static void uart_isr(const struct device *dev, void *user_data)
+// {
+// 	uart_irq_update(dev);
 
-	/* RX: 逐字节读出放入 ring_buf */
-	while (uart_irq_rx_ready(dev)) {
-		uint8_t ch;
-		if (uart_fifo_read(dev, &ch, 1) == 1) {
-			ring_buf_put(&rx_ring, &ch, 1);
-		}
-	}
+// 	/* RX: 逐字节读出放入 ring_buf */
+// 	while (uart_irq_rx_ready(dev)) {
+// 		uint8_t ch;
+// 		if (uart_fifo_read(dev, &ch, 1) == 1) {
+// 			ring_buf_put(&rx_ring, &ch, 1);
+// 		}
+// 	}
 
-	if (uart_irq_tx_ready(dev)) {
-        send_next_data();
-    }
-}
+// 	if (uart_irq_tx_ready(dev)) {
+//         send_next_data();
+//     }
+// }
